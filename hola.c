e884720ed28523911cf93222e5f676a5d6159219
ESTEBAN ADAUTO <adauto2597@gmail.com>
2026-09-12 23:21:44 -0500

    clase 1

--- hola.c
+++ hola.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
 
-int main()
-{
-    printf("hola a todos");
-printf("bienvenidos a C\");
-    intf("bienvenidos a C\");
-    printf("cuarta linea \n");
-    printf("quinta linea \n");
-    printf("sexta linea \n");
-    printf("septima linea \n");
-    retunr 0;
+int main(void){
+    printf("===Sin signo ===\n");
+
+    
+    uint8_t brillo_led = 23;
+    uint16_t contador_rpm = 1543;
+    uint32_t tiempo_ejecucion = 723873;
+
+    printf("variable Brillo led: %d\n", brillo_led);
+    printf("Tamaño de brillo_led: %zu bytes\n", sizeof(brillo_led));
+
+    printf("variable Contador RPM: %d\n", contador_rpm);
+    printf("Tamaño de contador_rpm: %zu bytes\n", sizeof(contador_rpm));
+
+    printf("variable Tiempo de ejecución: %d\n", tiempo_ejecucion);
+    printf("Tamaño de tiempo_ejecucion: %zu bytes\n", sizeof(tiempo_ejecucion));
+
+
+
+    printf("\n===Con signo ===\n");
+ 
+
+
+
+
+
+    return 0;
 }
